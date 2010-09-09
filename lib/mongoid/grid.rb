5fbdc4e34741cfed8c0a61d4391594c03ba1e4bf
Steve Sloan <steve@finagle.org>
2010-09-09 14:52:18 -0700

    Added HTTP caching logic.

--- lib/mongoid/grid.rb
+++ lib/mongoid/grid.rb
@@ -2,14 +2,14 @@ require 'mime/types'
 require 'mongoid'
 module Mongoid
   module Grid
-    
+
     def self.included(base)
       base.send(:extend,  ClassMethods)
       base.send(:include, InstanceMethods)
     end
-  
+
     module ClassMethods
-      
+
       ##
       # Declare an attachment for the object
       #
@@ -52,7 +52,7 @@ module Mongoid
             send(:delete_attachment, name, send("#{name}_id"))
           end
         end
-        
+
         ##
         # Return the relative URL to the file for use with Rack::Grid
         # eg: /grid/4ba69fde8c8f369a6e000003/somefile.png
@@ -63,50 +63,50 @@ module Mongoid
         end
 
       end
-      
+
       ##
-      # Accessor to GridFS 
+      # Accessor to GridFS
       def grid
         @grid ||= Mongo::Grid.new(Mongoid.database)
       end
-      
+
       ##
       # All the attachments types for this class
       def attachment_types
         @attachment_types ||= []
       end
-      
+
     end
 
     module InstanceMethods
-      
+
       private
       ##
       # Accessor to GridFS
       def grid
         @grid ||= self.class.grid
       end
-      
+
       ##
       # Holds queue of attachments to create
       def create_attachment_queue
         @create_attachment_queue ||= {}
       end
-      
+
       ##
       # Holds queue of attachments to delete
       def delete_attachment_queue
         @delete_attachment_queue ||= {}
       end
-      
+
       ##
       # Attachments we need to add after save.
       def create_attachment(name,file)
         if file.respond_to?(:read)
-          filename = file.respond_to?(:original_filename) ? 
+          filename = file.respond_to?(:original_filename) ?
                      file.original_filename : File.basename(file.path)
           type = MIME::Types.type_for(filename).first
-          mime = type ? type.content_type : "application/octet-stream" 
+          mime = type ? type.content_type : "application/octet-stream"
           send("#{name}_id=",   BSON::ObjectId.new)
           send("#{name}_name=", filename)
           send("#{name}_size=", File.size(file))
@@ -114,19 +114,19 @@ module Mongoid
           create_attachment_queue[name] = file
         end
       end
-      
+
       ##
       # Save an attachment to GridFS
       def create_grid_attachment(name,file)
         grid.put(
-          file.read, 
+          file.read,
           :filename => attributes["#{name}_name"],
           :content_type => attributes["#{name}_type"],
           :_id => attributes["#{name}_id"]
         )
         create_attachment_queue.delete(name)
       end
-      
+
       ##
       # Attachments we need to remove after save
       def delete_attachment(name,id)
@@ -136,20 +136,20 @@ module Mongoid
         send("#{name}_size=", nil)
         send("#{name}_type=", nil)
       end
-      
+
       ##
       # Delete an attachment from GridFS
       def delete_grid_attachment(name,id)
         grid.delete(id) if id.is_a?(BSON::ObjectId)
         delete_attachment_queue.delete(name)
       end
-      
+
       ##
       # Create attachments marked for creation
       def create_attachments
         create_attachment_queue.each {|k,v| create_grid_attachment(k,v)}
       end
-      
+
       ##
       # Delete attachments marked for deletion
       def delete_attachments
@@ -159,11 +159,10 @@ module Mongoid
       ##
       # Deletes all attachments from document
       def destroy_attachments
-        self.class.attachment_types.each do |name| 
+        self.class.attachment_types.each do |name|
           delete_attachment(name, send("#{name}_id"))
         end
       end
-      
     end
   end
 end
--- lib/rack/grid.rb
+++ lib/rack/grid.rb
@@ -24,6 +24,8 @@ module Rack
       @username   = options['username']
       @password   = options['password']
       @db         = options['db']
+
+      @cache_control = options['cache_control']
     end
 
     def db
@@ -37,7 +39,8 @@ module Rack
     # like /grid/4ba69fde8c8f369a6e000003/filename.jpg to find the file
     # with an id of 4ba69fde8c8f369a6e000003.
     def call(env)
-      request = Rack::Request.new(env)
+      @env = env
+      request = Rack::Request.new(@env)
       if request.path_info =~ /^\/#{prefix}\/(\w+).*$/
         grid_request($1)
       else
@@ -49,7 +52,20 @@ module Rack
     # Get file from GridFS or return a 404
     def grid_request(id)
       file = Mongo::Grid.new(db).get(BSON::ObjectId.from_string(id))
-      [200, {'Content-Type' => file.content_type}, [file.read]]
+
+      etag, last_modified = file.instance_variable_get(:@md5), Time.at( file.upload_date.to_i )
+      headers = {
+        'Content-Type' => file.content_type,
+        'ETag' => "\"#{etag}\"",
+        'Last-Modified' => last_modified.httpdate,
+        'Cache-Control' => cache_control_header,
+      }
+
+      if not_modified?( etag, last_modified )
+        [304, headers, 'Not Modified']
+      else
+        [200, headers, [file.read]]
+      end
     rescue Mongo::GridError, BSON::InvalidObjectId
       [404, {'Content-Type' => 'text/plain'}, ['File not found.']]
     end
@@ -64,5 +80,38 @@ module Rack
       raise ConnectionError, "Timeout connecting to GridFS (#{e.to_s})"
     end
 
+    DEFAULT_CACHE_CONTROL = "max-age=0, private, must-revalidate"
+    def cache_control_header
+      if @cache_control.blank?
+        DEFAULT_CACHE_CONTROL
+
+      elsif @cache_control[:no_cache]
+        'no-cache'
+
+      else
+        extras  = @cache_control[:extras]
+        max_age = @cache_control[:max_age]
+
+        options = []
+        options << "max-age=#{max_age.to_i}"  if max_age
+        options << (@cache_control[:public] ? 'public' : 'private')
+        options << 'must-revalidate'  if @cache_control[:must_revalidate]
+        options.concat(extras) if extras
+
+        options.join(', ')
+      end
+    end
+
+    def not_modified?( etag, last_modified )
+      if_none_match = @env['HTTP_IF_NONE_MATCH']
+      if if_modified_since = @env['HTTP_IF_MODIFIED_SINCE']
+        if_modified_since = Time.rfc2822( if_modified_since ) rescue nil
+      end
+
+      not_modified = if_none_match.present? || if_modified_since.present?
+      not_modified &&= (if_none_match == "\"#{etag}\"")       if if_none_match && etag
+      not_modified &&= (if_modified_since >= last_modified)   if if_modified_since && last_modified
+      not_modified
+    end
   end
 end
